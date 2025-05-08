7b2ac73875ffb0132e0b2202d669243dfb947ec1
BastianThoma <ossi_1993@hotmail.de>
2025-05-08 13:42:00 +0200

    add authentication guard to routes for protected access

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import type { NavigationGuardNext, RouteLocationNormalized } from 'vue-router';
+import { auth } from '@/firebase/config'
+
 
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
@@ -7,16 +10,25 @@ import AddMemory from '@/views/AddMemory.vue'
 import RandomMemory from '@/views/RandomMemory.vue'
 import Memories from '@/views/Memories.vue'
 
-const routes = [
+let requireAuth = (to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
+  let user = auth.currentUser;
+  if (!user) {
+    next('/login');
+  } else {
+    next();
+  }
+};
+
+let routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
-  { path: '/add', name: 'AddMemory', component: AddMemory },
-  { path: '/random', name: 'RandomMemory', component: RandomMemory },
-  { path: '/memories', name: 'Memories', component: Memories },
+  { path: '/add', name: 'AddMemory', component: AddMemory, beforeEnter: requireAuth },
+  { path: '/random', name: 'RandomMemory', component: RandomMemory, beforeEnter: requireAuth },
+  { path: '/memories', name: 'Memories', component: Memories, beforeEnter: requireAuth },
 ]
 
-const router = createRouter({
+let router = createRouter({
   history: createWebHistory(),
   routes
 })
